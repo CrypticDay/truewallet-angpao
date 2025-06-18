577102bbc02a329028ef99a26bb320469671d549
CrypticDay <81863374+CrypticDay@users.noreply.github.com>
2025-06-18 18:15:58 +0700

    1.0.6

--- src/truewallet-angpao.d.ts
+++ src/truewallet-angpao.d.ts
@@ -1,9 +1,1 @@
-declare module 'truewallet-angpao' {
-    interface RedeemResult {
-        status: 'SUCCESS' | 'FAILED' | 'CORS_ERROR' | string;
-        amount?: number;
-        message?: string;
-    }
-
-    export function clientRedeemAngpao(phone: string, code: string): Promise<RedeemResult>;
-}
+declare module 'truewallet-angpao';
